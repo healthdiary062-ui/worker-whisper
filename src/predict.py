4cb4d3f7078bb04e5568f70b18297cad4728e941
healthdiary062-ui <healthdiary062@gmail.com>
2025-12-09 13:04:39 +0500

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -36,7 +36,8 @@ class Predictor:
                 print(f"Model {model_name} could not be found.")
                 return None, None
 
-        model_names = ["tiny", "base", "small", "medium", "large-v1", "large-v2"]
+        model_names = ["tiny", "base", "small", "medium", "large-v1", "large-v2", "large-v3"]
+
         with ThreadPoolExecutor() as executor:
             for model_name, model in executor.map(load_model, model_names):
                 if model_name is not None:
